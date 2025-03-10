ce4448dee4d8144fb2f362912824ec7425e02164
Meir <tomeir2105@gmail.com>
2025-03-10 18:26:49 +0200

    Create vars.py

---
+++ PRACTICE P30/vars.py
@@ -0,0 +1,11 @@
+# create variable for holding your name
+my_name = "Meir"
+
+# create variable for holding your age
+my_age = "45"
+
+# create variable for holding your pets name
+my_pet_name = "Maple"
+
+use print() function to print them all.
+print(f"My name is {my_name}, I am {my_age} years old, and my pet's name is {my_pet_name}.")
