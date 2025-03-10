69a6c788308b81ec44076146ad1fe94cc6a665f4
Meir <tomeir2105@gmail.com>
2025-03-10 18:57:02 +0200

    Create vars2.py

---
+++ PRACTICE P39/vars2.py
@@ -0,0 +1,19 @@
+# create variable for holding your spouse (ife/husband) name
+spouse_name = "Gambit"
+
+# check and print the type of that variable.
+print(type(spouse_name))
+if (type(spouse_name) == str):
+  print("spouse name type is string")
+
+# create variable for holding your spouses age
+spouse_age = 25
+
+# convert age variable to type string
+spouse_age_str = str(spouse_age)
+
+# create variable to hold your credit card details
+credit_card = "xxxx-xxxx-xxxx-xxxx"
+
+# print() them away.
+print(f"my credit card number is {credit_card})
