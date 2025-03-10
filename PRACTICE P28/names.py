735b78f6db0488c10966e36050099b41ae0166fa
Meir <tomeir2105@gmail.com>
2025-03-10 18:23:53 +0200

    Create names.py

---
+++ PRACTICE P28/names.py
@@ -0,0 +1,8 @@
+# Define variable for your name with value of your name
+my_name = "Meir"
+
+# Define variable of your nickname with value of your nickname
+my_nickname = "The Rock"
+
+# Define variable of your pet,(children can be considered as pets) with value of your pets name
+my_pet_name = "Chase"
